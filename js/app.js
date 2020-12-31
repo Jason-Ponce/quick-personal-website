16eff95e3eef9d6195090a1d638e74edfedafb39
Jason P <jasonponcework@gmail.com>
2020-12-31 00:19:54 -0500

    highlight active tab js functionality

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ document.getElementById("about").addEventListener("click", swapViews);
 
 function swapViews(){
     const navEl = this;
+    console.log(this);
     const navList = {
         home: "home",
         resume: "resume",
@@ -14,15 +15,26 @@ function swapViews(){
     hideViews(navList);
     const el = document.getElementById(navEl.id+"-section");
     el.style.display = "block";
+    highlightNavEl(navEl);
 }
 
 function hideViews(navList){
     for (const nav in navList){
         const element = document.getElementById(nav+"-section");
-        console.log(element);
         if (element.style.display = "block"){
             element.style.display = "none"
-            console.log("Running at: "+nav);
+            removeHighlight(nav);
         }
     }
+}
+
+function highlightNavEl(el){
+    el.style.borderBottom = "3px solid #C8D5B9";
+    el.style.paddingBottom = "3px";
+}
+
+function removeHighlight(el){
+    console.log("removeHighlight: " + el);
+    const navEl = document.getElementById(el);
+    navEl.style.borderBottom = "initial";
 }
